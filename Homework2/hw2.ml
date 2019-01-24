f885788baa0e8d76ee566755b7f071da788ac320
Ryan Miyahara <rmiyahara144@gmail.com>
2019-01-23 21:06:01 -0800

    Began Homework2

---
+++ Homework2/hw2.ml
@@ -0,0 +1,9 @@
+(*
+* NAME: Ryan Miyahara
+* ID: 804585999
+* EMAIL: rmiyahara144@gmail.com
+*)
+
+let convert_grammar =
+
+let rec parse_tree_leaves tree =
---
+++ Homework2/hw2test.ml
@@ -0,0 +1,5 @@
+(*
+* NAME: Ryan Miyahara
+* ID: 804585999
+* EMAIL: rmiyahara144@gmail.com
+*)
